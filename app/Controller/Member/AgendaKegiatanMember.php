f54c6796a76e1f130dae14b7c3cac11942ce318d
Ridla Fadilah <ridlafadilah@users.noreply.github.com>
2017-03-29 23:35:39 +0700

    agenda panitia

--- app/Controller/Member/AgendaKegiatanMember.php
+++ app/Controller/Member/AgendaKegiatanMember.php
@@ -38,7 +38,7 @@ class AgendaKegiatanMember {
         $data = new TransactionActivity();
 //        $userMain = new MasterUserMain();
         if ($_POST['per_page'] == "") {
-            $Datatable->per_page = 5;
+            $Datatable->per_page = 10;
         } else {
             $Datatable->per_page = $_POST['per_page'];
         }
